47b00b4be4034f16a8b3698ac14464df324ae1b1
unknown <acaedric@gmail.com>
2021-12-03 18:54:48 -0500

    Primer commit

---
+++ create.js
@@ -0,0 +1,22 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      // The attributes of the item to be created
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      nombres: data.nombres, // Parsed from request body
+      apellidos: data.apellidos, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
