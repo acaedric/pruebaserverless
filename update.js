7ace34844eb5a1e8ee5b634f0aff6dccee1c5faa
unknown <acaedric@gmail.com>
2021-12-03 22:30:05 -0500

    arreglando la funcion update

--- update.js
+++ update.js
@@ -12,10 +12,10 @@ export const main = handler(async (event, context) => {
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET nombres = :nombres, apellidos = :apellidos",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
-      ":content": data.content || null,
+      ":nombres": data.nombres || null,
+      ":apellidos": data.apellidos || null,
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
